0e285d2376b771f37c3954a24513c92f6418bb40
yogendra-yatnalkar <yogenyat@gmail.com>
2020-06-04 16:39:53 +0530

    kept a safe copy of keyboard capture

--- src/capture_data/safe_keyboard_capture.py
+++ src/capture_data/safe_keyboard_capture.py
@@ -5,7 +5,12 @@ import time
 import pandas as pd
 from save_file import SaveFile
 from screen_capture import ScreenCapture
-from utility import Utility
+
+# Future utility packages
+from pyautogui import screenshot
+import numpy as np
+import cv2
+import os
 
 class KeyboardCapture:
 
@@ -22,15 +27,15 @@ class KeyboardCapture:
         # sv_file_obj --> save file object
 
     def on_press(self, key):
-        # if(key != keyboard.Key.esc):
         self.key_pressed.add(key)
         sc_grab = self.sc_cap_obj.image_grab() # screen grab
         img_name = self.sv_file_obj.save_image(sc_grab)  # save the screen to disk
+        
         last_row = len(self.action_df)
+        print(self.action_df)
         print(last_row)
-        self.action_df.loc[last_row] = [img_name,self.key_pressed]
+        self.action_df.loc[len(self.action_df)] = [img_name,self.key_pressed]
         print(img_name,self.key_pressed)
-        print('\n',self.action_df)
 
     def on_release(self,key):
         if(key == keyboard.Key.esc):
@@ -40,13 +45,26 @@ class KeyboardCapture:
         if(key in self.key_pressed):
             self.key_pressed.remove(key)
 
+def get_coordinates():
+    screen = np.array(screenshot())
+    screen = cv2.cvtColor(screen,cv2.COLOR_RGB2BGR)
+    # roi = cv2.selectROI(screen)
+    x0, y0, w, h = cv2.selectROI(screen) 
+    #returns the top-left corner coordinates and the width and height of the roi
+    cv2.destroyAllWindows()
+    x1 = x0 + w
+    y1 = y0 + h
+    return (x0,y0,x1,y1)
+
 if __name__ == "__main__":
     start_time = time.time()
-    util = Utility()
-    x0,y0,x1,y1 = util.get_coordinates() #returns 4 coordinates
-    # The module will start after a defined dalay
-    delay_time = 3
-    util.generate_delay(delay_time)
+    x0,y0,x1,y1 = get_coordinates()
+
+    # The module will start after a delay of 10 sec
+    for i in range(4,0,-1):
+        print("The program will start in : ",i)
+        time.sleep(1)
+
     save_img_path = 'D:/Yogendra D/AI_for_any_game_using_CNN/src/game_dataset'
     obj = KeyboardCapture(x0,y0,x1,y1,save_img_path)
 
@@ -57,7 +75,6 @@ if __name__ == "__main__":
         listener.run()
     except BaseException as error:
         print("Unexpected error : ",error)
-        print("The current created dataset will be cleared")
     
     end_time = time.time()
     print("Time required for execution : ", end_time - start_time)
